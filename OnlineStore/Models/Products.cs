be71dda8e9545d7d9383fb29cf42f983a9d4ea7a
DarthShadow147 <53203282+DarthShadow147@users.noreply.github.com>
2020-03-25 12:05:09 -0500

    Archivos pendientes

---
+++ OnlineStore/Models/Products.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Models
+{
+    public class Products
+    {
+        public int IdProduct { get; set; }
+    }
+}
